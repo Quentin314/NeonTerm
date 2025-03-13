ad7d1bc1d3e8ea6625cd320b5a7b4a4741fa066a
bigbug314 <bigbug31415@gmail.com>
2025-03-13 15:01:34 +0100

    NeonTerm struct

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 mod renderer;
 use std::time::Instant;
-use crossterm;
 use noise::{Perlin, NoiseFn};
 
 fn main() {
-    let mut frame_count = 0;
-    // Disable the cursor
-    crossterm::execute!(std::io::stdout(), crossterm::cursor::Hide).unwrap();
+    // Create the NeonTerm renderer
+    let mut term = renderer::NeonTerm::new((100, 100), (0, 0));
     // Create a noise generator with a random seed
     let perlin = Perlin::new(42);
         
     // Animation parameters
-    let scale = 0.03; // Controls how zoomed in/out the noise pattern is
+    let scale = 0.005; // Controls how zoomed in/out the noise pattern is
     let speed = 2.0; // Controls animation speed
     
     let start_time = Instant::now();
-    renderer::clear();
+    let mut frame_count = 0;
     loop {
         // Get the terminal size
-        let (width, height) = renderer::get_terminal_size();
+        let (width, height) = renderer::NeonTerm::get_term_size();
         let size = width.min(height*2-4);
+        term.update_size(size, size);
         // Update time - we'll use this as our z-coordinate in the noise function
         let time = start_time.elapsed().as_secs_f64() * speed;
-        let mut grid: Vec<(u8, u8, u8)> = vec![(0, 0, 0); size * size];
         // Generate new colors for the entire grid
         for y in 0..size {
             for x in 0..size {
@@ -41,15 +39,13 @@ fn main() {
                 let g = ((g_value * 0.5 + 0.5) * 255.0) as u8;
                 let b = ((b_value * 0.5 + 0.5) * 255.0) as u8;
                 
-                // Store the color in our grid
-                grid[y * size + x] = (r, g, b);
+                // Store the color in the terminal buffer
+                term.buffer[y * size + x] = (r, g, b);
             }
         }
-        // Convert the pixels to an ANSI string
-        let ansi = renderer::to_ansi(&mut grid, size, size);
 
-        // Print the ANSI string
-        renderer::overwrite(ansi);
+        // Render
+        term.render();
 
         // Print the frame count
         println!("Frame: {}", frame_count);
--- src/renderer.rs
+++ src/renderer.rs
@@ -2,45 +2,77 @@ use crossterm;
 
 use std::fmt::Write;
 
-pub fn to_ansi(pixels: &mut Vec<(u8, u8, u8)>, width: usize, mut height: usize) -> String {
-    // Renders the pixels to the terminal using half blocks
-    // If the height is odd, add an extra row of black pixels
-    if height % 2 == 1 {
-        pixels.append(&mut vec![(0, 0, 0); width]);
-        height += 1;
+#[derive(Debug, Clone)]
+pub struct NeonTerm {
+    pub buffer: Vec<(u8, u8, u8)>,
+    size: (usize, usize),
+    offset: (usize, usize)
+}
+
+impl NeonTerm {
+    pub fn new(size: (usize, usize), offset: (usize, usize)) -> Self {
+        let buffer = vec![(0, 0, 0); size.0 * size.1];
+        crossterm::execute!(std::io::stdout(), crossterm::cursor::Hide).unwrap();
+        NeonTerm::clear();
+        return NeonTerm { buffer, size, offset };
     }
 
-    // Create a big string with everything to print
-    let mut output = String::with_capacity(height * width * 25);
-    for y in 0..height/2 {
-        for x in 0..width {
-            // Background color : pixels[(y*2) * width + x]
-            // Foreground color : pixels[(y*2+1) * width + x]
-            let bg = pixels[(y*2) * width + x];
-            let fg = pixels[(y*2+1) * width + x];
+    pub fn render(&mut self) {
+        self.overwrite();
+    }
 
-            write!(output, 
-                   "\x1b[48;2;{};{};{}m\x1b[38;2;{};{};{}m▄", 
-                   bg.0, bg.1, bg.2, fg.0, fg.1, fg.2)
-                   .unwrap();
+
+    fn to_ansi(pixels: &mut Vec<(u8, u8, u8)>, width: usize, mut height: usize) -> String {
+        // Renders the pixels to the terminal using half blocks
+        // If the height is odd, add an extra row of black pixels
+        if height % 2 == 1 {
+            pixels.append(&mut vec![(0, 0, 0); width]);
+            height += 1;
         }
-        write!(output, "\x1b[0m\n").unwrap();
+    
+        // Create a big string with everything to print
+        let mut output = String::with_capacity(height * width * 25);
+        for y in 0..height/2 {
+            for x in 0..width {
+                // Background color : pixels[(y*2) * width + x]
+                // Foreground color : pixels[(y*2+1) * width + x]
+                let bg = pixels[(y*2) * width + x];
+                let fg = pixels[(y*2+1) * width + x];
+    
+                write!(output, 
+                       "\x1b[48;2;{};{};{}m\x1b[38;2;{};{};{}m▄", 
+                       bg.0, bg.1, bg.2, fg.0, fg.1, fg.2)
+                       .unwrap();
+            }
+            write!(output, "\x1b[0m\n").unwrap();
+        }
+        return output;
     }
-    return output;
-}
 
-pub fn get_terminal_size() -> (usize, usize) {
-    // Get the size of the terminal
-    let size = crossterm::terminal::size().unwrap();
-    return (size.0 as usize, size.1 as usize);
-}
+    fn overwrite(&mut self) {
+        print!("\x1b[1;1H{}", NeonTerm::to_ansi(&mut self.buffer, self.size.0, self.size.1));
+    }
 
+    pub fn get_term_size() -> (usize, usize) {
+        // Get the size of the terminal
+        let size = crossterm::terminal::size().unwrap();
+        return (size.0 as usize, size.1 as usize);
+    }
 
-pub fn overwrite(ansi:String) {
-    print!("\x1b[1;1H{}", ansi);
-}
+    pub fn get_size(&self) -> (usize, usize) {
+        return self.size;
+    }
 
+    pub fn clear() {
+        print!("\x1b[2J");
+    }
 
-pub fn clear() {
-    print!("\x1b[2J");
+    pub fn update_size(&mut self, width: usize, height: usize) {
+        if self.size == (width, height) {
+            return;
+        }
+        self.size = (width, height);
+        self.buffer = vec![(0, 0, 0); width * height];
+        NeonTerm::clear();
+    }
 }
